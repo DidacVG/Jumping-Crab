2c3f377866dd8386af35e5c57758eedcec88d74c
DidacVG <didac.valles@enti.cat>
2025-04-21 17:45:32 +0200

    jsdkjahjaik

---
+++ Assets/Scripts/Movimiento_Cangrjo.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento_Cangrjo : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    private Rigidbody2D Rigidbody2D;
+    private float Horizontal;
+    private bool Grounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+
+        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.blue);
+
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        {
+            Grounded = true;
+        }
+        else Grounded = false;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
+    }
+}
